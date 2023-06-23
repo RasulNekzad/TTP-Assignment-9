0214b4e80931f7e0908d5c7b125986c32ad17771
rasulnekzad <rasulnekzad@Rasuls-MacBook-Air.local>
2023-06-22 22:27:44 -0400

    more formatting

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { React, useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 import Home from "./components/Home";
 import Debits from "./components/Debits";
 import Credits from "./components/Credits";
 
 function App() {
   const [debits, setDebits] = useState(0);
-    const [credits, setCredits] = useState(0);
-    const [accountBalance, setAccountBalance] = useState(0);
-    const [debitsDescription, setDebitsDescription] = useState("");
-    const [creditsDescription, setCreditsDescription] = useState("");
+  const [credits, setCredits] = useState(0);
+  const [accountBalance, setAccountBalance] = useState(0);
+  const [debitsDescription, setDebitsDescription] = useState("");
+  const [creditsDescription, setCreditsDescription] = useState("");
 
-    async function fetchDebits() {
-        const result = await axios.get("https://bank-of-react-b745wfs0u-ajlapid718.vercel.app/debits");
-        setDebits(result.data);
-    }
+  async function fetchDebits() {
+    const result = await axios.get(
+      "https://bank-of-react-b745wfs0u-ajlapid718.vercel.app/debits"
+    );
+    setDebits(result.data);
+  }
 
-    async function fetchCredits() {
-        const result = await axios.get("https://bank-of-react-b745wfs0u-ajlapid718.vercel.app/credits");
-        setCredits(result.data);
-    }
+  async function fetchCredits() {
+    const result = await axios.get(
+      "https://bank-of-react-b745wfs0u-ajlapid718.vercel.app/credits"
+    );
+    setCredits(result.data);
+  }
 
-    useEffect(() => {fetchDebits()}, [])
-    useEffect(() => {fetchCredits()}, [])
+  useEffect(() => {
+    fetchDebits();
+  }, []);
+  useEffect(() => {
+    fetchCredits();
+  }, []);
 
-    const updateAccountBalance = () => {
-        setAccountBalance(credits-debits);
-    }
+  const updateAccountBalance = () => {
+    setAccountBalance(credits - debits);
+  };
 
-    useEffect(() => {updateAccountBalance()}, [credits,debits])
-  
+  useEffect(() => {
+    updateAccountBalance();
+  }, [credits, debits]);
+
   return (
     <Router>
       <div className="App">
@@ -50,9 +60,42 @@ function App() {
         </nav>
 
         <Routes>
-          <Route path="/debits" element={<Debits debits={debits} accountsBalance={accountBalance} setDebits={setDebits} debitsDescription={debitsDescription} setDebitsDescription={setDebitsDescription}/>}></Route>
-          <Route path="/credits" element={<Credits credits={credits} accountsBalance={accountBalance} setCredits={setCredits} creditsDescription={creditsDescription} setCreditsDescription={setCreditsDescription}/>}></Route>
-          <Route path="/*" element={<Home debits={debits} credits={credits} accountBalance={accountBalance} debitsDescription={debitsDescription} creditsDescription={creditsDescription}/>}></Route>
+          <Route
+            path="/debits"
+            element={
+              <Debits
+                debits={debits}
+                accountsBalance={accountBalance}
+                setDebits={setDebits}
+                debitsDescription={debitsDescription}
+                setDebitsDescription={setDebitsDescription}
+              />
+            }
+          ></Route>
+          <Route
+            path="/credits"
+            element={
+              <Credits
+                credits={credits}
+                accountsBalance={accountBalance}
+                setCredits={setCredits}
+                creditsDescription={creditsDescription}
+                setCreditsDescription={setCreditsDescription}
+              />
+            }
+          ></Route>
+          <Route
+            path="/*"
+            element={
+              <Home
+                debits={debits}
+                credits={credits}
+                accountBalance={accountBalance}
+                debitsDescription={debitsDescription}
+                creditsDescription={creditsDescription}
+              />
+            }
+          ></Route>
         </Routes>
       </div>
     </Router>
